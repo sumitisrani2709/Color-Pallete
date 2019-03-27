f0b7f38569a0cd5d708d8ade0cb7deee48579a8f
Sumit Israni <sisrani@grepruby.io>
2019-03-27 14:45:01 +0530

    Added a functionality to create a color.

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -5,4 +5,25 @@ class ColorsController < ApplicationController
   def index
     @colors = Color.paginate(page: params[:page], per_page: 10)
   end
+
+  def new
+    @color = Color.new
+  end
+
+  def create
+    @color = Color.new(permit_params)
+    if @color.save
+      flash[:success] = 'Color has been created sucessfully.'
+      redirect_to colors_path
+    else
+      flash[:error] = @color.errors.full_messages.join('<br>').html_safe
+      render :new
+    end
+  end
+
+  private
+
+  def permit_params
+    params.require(:color).permit(:name, :code)
+  end
 end
