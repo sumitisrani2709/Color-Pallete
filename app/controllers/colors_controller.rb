2efb5be02900d3c7b3ef16acc35fa3b9a0059939
Sumit Israni <sisrani@grepruby.io>
2019-03-27 14:57:33 +0530

    Added the functionality of edit a color.

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -2,6 +2,7 @@
 
 # This is a colors controller
 class ColorsController < ApplicationController
+  before_action :find_color, only: %w[edit update]
   def index
     @colors = Color.paginate(page: params[:page], per_page: 10)
   end
@@ -21,9 +22,28 @@ class ColorsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @color.update_attributes(permit_params)
+      flash[:success] = 'Color has been updated sucessfully.'
+      redirect_to colors_path
+    else
+      flash[:error] = @color.errors.full_messages.join('<br>')
+      render :edit
+    end
+  end
+
   private
 
   def permit_params
     params.require(:color).permit(:name, :code)
+  end
+
+  def find_color
+    @color = Color.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:notice] = 'The requested resource does not exist.'
+    redirect_to(colors_path)
   end
 end
